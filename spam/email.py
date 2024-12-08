b69da1c9d25bf74d56287daed5c3efdf30d83911
maxbalrog <maxbalrog@gmail.com>
2024-12-08 16:09:11 +0100

    add additional repo structure for sys.path discussion

--- spam/email.py
+++ spam/email.py
@@ -5,6 +5,16 @@ To: {}
 Are you free???
 '''
 
+invite_template = '''
+Dear {},
+
+We invite You to the next CHIPS seminar!
+'''
+
 def send_spam_email(email='my-email@uni-jena.de'):
     spam = spam_template.format(email)
     print(spam)
+
+def send_invitation(name):
+    invite = invite_template.format(name)
+    print(invite)
