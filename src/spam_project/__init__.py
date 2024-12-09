b7b868276ca25c615807401fb9ce60f4597ba038
maxbalrog <maxbalrog@gmail.com>
2024-12-09 20:07:23 +0100

    change imports in core __init__.py file

---
+++ src/spam_project/__init__.py
@@ -0,0 +1,3 @@
+from spam_project import send_spam
+from spam_project.spam import collections, email
+from spam_project.realdeal import call
--- src/spam_project/realdeal/call.py
+++ src/spam_project/realdeal/call.py
@@ -2,9 +2,10 @@ from spam_project.spam.email import send_invitation
 from spam_project.send_spam import print_spam
 import spam_project.spam.collections as spam_collections
 
-print_spam()
+if __name__ == '__main__':
+    print_spam()
 
-print('Sending invitation')
-send_invitation('Maksim')
+    print('Sending invitation')
+    send_invitation('Maksim')
 
-print(dir(spam_collections))
+    print(dir(spam_collections))
